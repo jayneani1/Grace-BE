c8077d2e95d63c8cd4a51042db6235a8407c1a31
jayneani <jayneaniello27@gmail.com>
2020-05-14 20:58:47 -0500

    mood

--- apps/graceBackend/models.py
+++ apps/graceBackend/models.py
@@ -12,7 +12,7 @@ class Entry(models.Model):
     )
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    mood = MultiSelectField(choices = MOOD_CHOICES)
+    mood = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_public = models.BooleanField(default=True)
