bc5d9b97bc3ab68341ba41a017705694b796b6eb
jayneani <jayneaniello27@gmail.com>
2020-05-15 00:13:23 -0500

    late

--- apps/graceBackend/urls.py
+++ apps/graceBackend/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import url
-from rest_framework.routers import DefaultRouter
-from .views import EntryViewSet, PublicEntries, PublicEntriesDetail
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import PublicEntries, PublicEntriesDetail, CreateEntry
 
-router = DefaultRouter()
-router.register('entries', EntryViewSet, basename='entries')
-
-custom_urlpatterns = [
+urlpatterns = [
+    url(r'^create-entry/$', CreateEntry.as_view(), name='create-entry'),
     url(r'^public-entries/$', PublicEntries.as_view(), name='public-entries'),
     url(r'^public-entries/(?P<pk>\d+)/$', PublicEntriesDetail.as_view(), name='public-entry-detail'),
 ]
 
-urlpatterns = router.urls
-urlpatterns += custom_urlpatterns
+urlpatterns = format_suffix_patterns(urlpatterns)
+
